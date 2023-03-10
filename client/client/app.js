9e63baee42db91666fcded5e96fda651504ef917
ProKafelek12e <pro.kafelek@gmail.com>
2023-03-10 15:59:59 +0100

    client functions done

---
+++ client/client/app.js
@@ -0,0 +1,31 @@
+var json =[]
+async function getKandydaci(){
+    const data = await fetch(`${baseurl}/kandydaci`)
+    json = await data.json()
+    await createKandydaci()
+}
+function createKandydaci(){
+    document.getElementById("kandydaci").innerHTML = ""
+    for(var i=0;i<=json.length-1;i++){
+        const div = document.createElement("div")
+        div.setAttribute("onclick",`vote(${i})`)
+        const h1 = document.createElement("h1")
+        h1.innerHTML = "Kandydat nr. "+(i+1)+": "
+        const p = document.createElement("P")
+        p.innerHTML = json[i].imie +" "+json[i].nazwisko
+        div.appendChild(h1)
+        div.appendChild(p)
+        document.getElementById("kandydaci").appendChild(div)
+    }
+}
+async function vote(i){
+    const imie = json[i].imie
+    const nazwisko = json[i].nazwisko
+    const Pesel = document.getElementById("inpPesel").value
+    if(Pesel=="") alert("Podaj pesel")
+
+    else {
+        await fetch(`${baseurl}/vote/${imie}/${nazwisko}/${Pesel}`)
+    }
+}
+getKandydaci()
--- server/index.js
+++ server/index.js
@@ -4,22 +4,36 @@ const mysql = require('mysql')
 
 const app = express()
 app.use(cors())
+
 var con = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"",
-    database:""
+    database:"votes"
 })
 
-con.connect({function(err){
+con.connect(function(err){
     if(err) console.log(err)
     console.log("connected to database")
-}})
-app.get("/",function(req,res){
-
 })
-app.get("/",function(req,res){
-
+app.get("/kandydaci",function(req,res){
+    const sql = "SELECT * FROM kandydaci"
+    con.query(sql,function(err,result,fields){
+        if(err) console.log(err)
+        res.send(result)
+        console.log(fields)
+    })
+})
+app.get("/vote/:imie/:nazwisko/:PESEL",function(req,res){
+    const nazwisko= req.params.nazwisko
+    const imie = req.params.imie
+    const pesel = req.params.PESEL
+    const sql = `INSERT INTO glosujacy (pesel, kandydat) VALUES ('${pesel}','${imie} ${nazwisko}')`
+    con.query(sql,function(err,result,fields){
+        if(err) console.log(err)
+        res.send("dodano")
+        console.log(fields)
+    })
 })
 
 app.listen(3000)
